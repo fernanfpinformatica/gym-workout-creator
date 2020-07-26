3fede01d1bf707f508f2312598b902c6469a5a2e
Fernando Arias <fernan.fpinformatica@gmail.com>
2020-07-26 07:25:53 +0200

    Add images categorized and reference statically from assets

--- src/App.vue
+++ src/App.vue
@@ -63,12 +63,12 @@ export default {
       ],
       colspan: 4,
       imgSrc: [
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492639926499-SY6KJ6TN3ZO5W53ZAVLB/ke17ZwdGBToddI8pDm48kA--CZeMMIZvCJ1et5mufG9Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVF1Hxwm206IZyO_-jfK4JJz9uvwlcFs25qkudxW7Q1inTbuQio6mPVtJnWWmEQSq6s/Flat+Barbell+Chest+Press",
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492675344310-NPZ7AD7KAENZR88289ZR/ke17ZwdGBToddI8pDm48kOu_umM5xYaUXSes57xj8BlZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVGj2fmHITMhmmP-HNrXPt0-KgIKUgpERTQ2FmiuRFqB0zn75LfaXcLI4pX9TKGf4Uw/Wide+Grip+Overhand+Chin+Ups",
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492640379861-4YARXK0C5XIKTU2LW2II/ke17ZwdGBToddI8pDm48kGphHHQBDgqAXFtoomGMlsRZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEkAsqeApjCRwKqMZmT-ct3icbxu9ZdyFLG1xSIp6h4TIjbgrpQzS2D1wsvCYhefSE/Seated+Dumbbell+Shoulder+Press",
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1496316249945-42XE9BZ2L9F7DH2K6HZ0/ke17ZwdGBToddI8pDm48kFJThvQFJS4gbAQUR3Eh5HRZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVE57Ad1LWFroL6iS1PVW-8kVfg4CWSwJuk9MRfwRwqxFhur-lC0WofN0YB1wFg-ZW0/Seated+Alternate+Incline+Dumbbell+Biceps+Curl",
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1493844683178-DZ1MAYUKOM1HJLTMYKFN/ke17ZwdGBToddI8pDm48kPV83lobj4YDXKov937Ci8FZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFvqqedpVUyeluM6UTyt0yR6cUu5seITZd_gvD35ZPUGZXleA9PsoOHujT9UMkA80c/One+Arm+Overhead+Standing+Triceps+Extensions",
-        "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1493084263075-NODSRKLSAPA8JS7IQP9Y/ke17ZwdGBToddI8pDm48kHWk_ixjWi6EbIpwdZqLlPVZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVGPiSYl-QlC9ki6sozNR8vUxJPSCvO27XorCMjdQnVjuKl4Ys1tHh8ZXOc-Io9Ug4o/Oblique+Raises",
+        require('./assets/img/Back+Lats/image--005.jpg'),
+        require('./assets/img/Back+Lats/image--005.jpg'),
+        require('./assets/img/Back+Lats/image--005.jpg'),
+        require('./assets/img/Back+Lats/image--005.jpg'),
+        require('./assets/img/Back+Lats/image--005.jpg'),
+        require('./assets/img/Back+Lats/image--005.jpg'),
       ],
       calloutData: [
         {
