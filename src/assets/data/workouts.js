1ae0fd2c01118d58e9966b5a8dd758086046201b
Fernando Arias <fernan.fpinformatica@gmail.com>
2020-07-26 16:48:06 +0200

    Workout divided in 7 days done (only 4 last days)

--- src/assets/data/workouts.js
+++ src/assets/data/workouts.js
@@ -1,5 +1,5 @@
 const workouts = {
-  torsoPierna: {
+  dividedRoutineDay4: {
     workoutTableData: {
       titlesList: [
         "Ejercicio",
@@ -10,22 +10,22 @@ const workouts = {
       weightColumnColespan: 4,
       exercises: [
         {
-          imgSrc: require('./../img/Back+Lats/image--007.jpg')
+          imgSrc: require('./../img/UpperLegs+Glutes/image--063.jpg')
         },
         {
-          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+          imgSrc: require('./../img/UpperLegs+Glutes/image--019.jpg')
         },
         {
-          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+          imgSrc: require('./../img/Forearms+Calves/image--012.jpg')
         },
         {
-          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+          imgSrc: require('./../img/Forearms+Calves/image--013.jpg')
         },
         {
-          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+          imgSrc: require('./../img/UpperLegs+Glutes/image--017.jpg')
         },
         {
-          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+          imgSrc: require('./../img/Back+Lats/image--041.jpg')
         },
       ]
     },
@@ -44,7 +44,145 @@ const workouts = {
         ]
       }
     ]
-  }
+  },
+  dividedRoutineDay5: {
+    workoutTableData: {
+      titlesList: [
+        "Ejercicio",
+        "Series-Rep",
+        "Descanso",
+        "Peso",
+      ],
+      weightColumnColespan: 4,
+      exercises: [
+        {
+          imgSrc: require('./../img/Back+Lats/image--005.jpg')
+        },
+        {
+          imgSrc: require('./../img/Back+Lats/image--045.jpg')
+        },
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--005.jpg')
+        },
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--014.jpg')
+        },
+        {
+          imgSrc: require('./../img/Shoulders/image--008.jpg')
+        },
+        {
+          imgSrc: require('./../img/Triceps/image--015.jpg')
+        },
+      ]
+    },
+    calloutData: [
+      {
+        title: "🔥 Calentamiento:",
+        listItems: [
+          "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+          "2 minutos de movilidad articular"
+        ]
+      },
+      {
+        title: "🤸 Después del entrenamiento:",
+        listItems: [
+          "Estiramientos de cada grupo muscular durante 8-10 minutos"
+        ]
+      }
+    ]
+  },
+  dividedRoutineDay6: {
+    workoutTableData: {
+      titlesList: [
+        "Ejercicio",
+        "Series-Rep",
+        "Descanso",
+        "Peso",
+      ],
+      weightColumnColespan: 4,
+      exercises: [
+        {
+          imgSrc: require('./../img/Chest/image--009.jpg')
+        },
+        {
+          imgSrc: require('./../img/Chest/image--011.jpg')
+        },
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--063.jpg')
+        },
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--019.jpg')
+        },
+        {
+          imgSrc: require('./../img/Biceps/image--030.jpg')
+        },
+        {
+          imgSrc: require('./../img/Triceps/image--015.jpg')
+        },
+      ]
+    },
+    calloutData: [
+      {
+        title: "🔥 Calentamiento:",
+        listItems: [
+          "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+          "2 minutos de movilidad articular"
+        ]
+      },
+      {
+        title: "🤸 Después del entrenamiento:",
+        listItems: [
+          "Estiramientos de cada grupo muscular durante 8-10 minutos"
+        ]
+      }
+    ]
+  },
+  dividedRoutineDay7: {
+    workoutTableData: {
+      titlesList: [
+        "Ejercicio",
+        "Series-Rep",
+        "Descanso",
+        "Peso",
+      ],
+      weightColumnColespan: 4,
+      exercises: [
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--024.jpg')
+        },
+        {
+          imgSrc: require('./../img/UpperLegs+Glutes/image--017.jpg')
+        },
+        {
+          imgSrc: require('./../img/Abs+Core/image--016.jpg')
+        },
+        {
+          imgSrc: require('./../img/Back+Lats/image--024.jpg')
+        },
+        {
+          imgSrc: require('./../img/Forearms+Calves/image--013.jpg')
+        },
+        {
+          imgSrc: require('./../img/Forearms+Calves/image--012.jpg')
+        },
+      ]
+    },
+    calloutData: [
+      {
+        title: "🔥 Calentamiento:",
+        listItems: [
+          "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+          "2 minutos de movilidad articular"
+        ]
+      },
+      {
+        title: "🤸 Después del entrenamiento:",
+        listItems: [
+          "Estiramientos de cada grupo muscular durante 8-10 minutos"
+        ]
+      }
+    ]
+  },
 }
 
   export default workouts;
