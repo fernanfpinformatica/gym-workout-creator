7a25d2bfbde260388313b047f3617f5680ed39df
Fernando Arias <fernan.fpinformatica@gmail.com>
2020-07-26 16:53:10 +0200

    Added Divided workout day 1 to routines

--- src/assets/data/workouts.js
+++ src/assets/data/workouts.js
@@ -1,4 +1,50 @@
 const workouts = {
+  dividedRoutineDay1: {
+    workoutTableData: {
+      titlesList: [
+        "Ejercicio",
+        "Series-Rep",
+        "Descanso",
+        "Peso",
+      ],
+      weightColumnColespan: 4,
+      exercises: [
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492686568706-KFJ3ATRWHGB815ZR95QC/ke17ZwdGBToddI8pDm48kEn1--keXipKm-4-idoUv1JZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEGXIRsulM5lSNpkm0M2UY_ihY2wQ3dDmf1qhKwgeeuLsRpFBYQPJjKYbbHIEz2HLg/Barbell+Back+Squats"
+        },
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492686351677-R6IKDDG4PVEQ6LLVXSPE/ke17ZwdGBToddI8pDm48kARqa-X33SlNaNw3JvNHHwVZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHfRcUcS1K8dSGKeej5DAmGMmCGWlus2V6ACj3-b4NBUpeuVmK9wgfnRXYYbJgKuCM/Leg+Extensions"
+        },
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1495712960400-76K6387DKCSSFT6OV7PU/ke17ZwdGBToddI8pDm48kAMzkXTlKFeGmrjuZNXNEGpZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVGaZLe-qj62sT0QIJrrOGu3Is5lVoRCMti8IPxsD5_39-87Nsj43NRAr6WuWZv5DKs/Barbell+Deadlifts"
+        },
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492686944120-1GFOTC3E69FESJYWJ4TK/ke17ZwdGBToddI8pDm48kAzjdvmcwTi2DX4rjsrajCFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHLp8qhwfVEdyv5A5nbUTGG0QwNSa8-hGa8-UB3zZ1lpX-5hmNjaqsYTvOQnMPB6NE/Lying+Hamstring+Curls"
+        },
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1492639926499-SY6KJ6TN3ZO5W53ZAVLB/ke17ZwdGBToddI8pDm48kA--CZeMMIZvCJ1et5mufG9Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVF1Hxwm206IZyO_-jfK4JJz9uvwlcFs25qkudxW7Q1inTbuQio6mPVtJnWWmEQSq6s/Flat+Barbell+Chest+Press"
+        },
+        {
+          imgSrc: "https://images.squarespace-cdn.com/content/v1/55e406fbe4b0b03c5e7543ae/1496867578002-TVIMGBGE0XUWKPPZPQ43/ke17ZwdGBToddI8pDm48kLZ8E23_BGVE1N4cWg4liNNZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEGYK8NzAv3CEfjp0StVGHXJ6o2CrLf4q9qkkbv8XtOWaQvevUbj177dmcMs1F0H-0/Seated+Side+Raises"
+        },
+      ]
+    },
+    calloutData: [
+      {
+        title: "🔥 Calentamiento:",
+        listItems: [
+          "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+          "2 minutos de movilidad articular"
+        ]
+      },
+      {
+        title: "🤸 Después del entrenamiento:",
+        listItems: [
+          "Estiramientos de cada grupo muscular durante 8-10 minutos"
+        ]
+      }
+    ]
+  },
   dividedRoutineDay4: {
     workoutTableData: {
       titlesList: [
