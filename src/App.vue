4480baf5e5400190609fea40704e8c2ff75b491f
Fernando Arias <fernan.fpinformatica@gmail.com>
2020-07-26 08:04:34 +0200

    Add workout as a component and parametrize workouts data in main comp.

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,115 @@
 <template>
   <div id="app">
-    <div class="workout-wrapper">
-      <WorkoutHeader/>
-
-      <div class="info-callout-wrapper">
-        <Callout
-          v-for="callout in calloutData"
-          :key="callout.title"
-          :title="callout.title"
-          :listItems="callout.listItems"
-        />
-      </div>
-
-      <ExerciseTable
-        :tableTitlesList="workoutTableData.titlesList"
-        :exerciseList="workoutTableData.exercises"
-        :weightColumnColespan=workoutTableData.weightColumnColespan
-      />
-    </div>
-    <div class="workout-wrapper">
-      <WorkoutHeader/>
-
-      <div class="info-callout-wrapper">
-        <Callout
-          v-for="callout in calloutData"
-          :key="callout.title"
-          :title="callout.title"
-          :listItems="callout.listItems"
-        />
-      </div>
-
-      <ExerciseTable
-        :tableTitlesList="workoutTableData.titlesList"
-        :exerciseList="workoutTableData.exercises"
-        :weightColumnColespan=workoutTableData.weightColumnColespan
-      />
-    </div>
+    <Workout
+      v-for="workout in workouts"
+      :key="workout"
+      :calloutData="workout.calloutData"
+      :workoutTableData="workout.workoutTableData"
+    />
   </div>
 </template>
 
 <script>
-import WorkoutHeader from './components/WorkoutHeader.vue'
-import Callout from './components/Callout.vue'
-import ExerciseTable from './components/ExerciseTable.vue'
+import Workout from './components/Workout.vue'
 
 export default {
   name: 'App',
   components: {
-    WorkoutHeader,
-    Callout,
-    ExerciseTable
+    Workout,
   },
   data () {
     return {
-      workoutTableData: {
-        titlesList: [
-          "Ejercicio",
-          "Series-Rep",
-          "Descanso",
-          "Peso",
-        ],
-        weightColumnColespan: 4,
-        exercises: [
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-          {
-            imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
-          },
-        ]
-      },
-      calloutData: [
+      workouts: [
         {
-          title: "🔥 Calentamiento:",
-          listItems: [
-            "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
-            "2 minutos de movilidad articular"
+          workoutTableData: {
+            titlesList: [
+              "Ejercicio",
+              "Series-Rep",
+              "Descanso",
+              "Peso",
+            ],
+            weightColumnColespan: 4,
+            exercises: [
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+            ]
+          },
+          calloutData: [
+            {
+              title: "🔥 Calentamiento:",
+              listItems: [
+                "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+                "2 minutos de movilidad articular"
+              ]
+            },
+            {
+              title: "🤸 Después del entrenamiento:",
+              listItems: [
+                "Estiramientos de cada grupo muscular durante 8-10 minutos"
+              ]
+            }
           ]
         },
         {
-          title: "🤸 Después del entrenamiento:",
-          listItems: [
-            "Estiramientos de cada grupo muscular durante 8-10 minutos"
+          workoutTableData: {
+            titlesList: [
+              "Ejercicio",
+              "Series-Rep",
+              "Descanso",
+              "Peso",
+            ],
+            weightColumnColespan: 4,
+            exercises: [
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--006.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--007.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--008.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+              {
+                imgSrc: require('./assets/img/Back+Lats/image--005.jpg')
+              },
+            ]
+          },
+          calloutData: [
+            {
+              title: "🔥 Calentamiento:",
+              listItems: [
+                "5 minutos de cardio (bicicleta estática, elíptica o carrera)",
+                "2 minutos de movilidad articular"
+              ]
+            },
+            {
+              title: "🤸 Después del entrenamiento:",
+              listItems: [
+                "Estiramientos de cada grupo muscular durante 8-10 minutos"
+              ]
+            }
           ]
         }
       ]
@@ -107,9 +123,5 @@ export default {
   font-family: Segoe UI, Frutiger, Frutiger Linotype, Dejavu Sans, Helvetica Neue, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-.workout-wrapper {
-  margin: 1rem 0 2.5rem;
 }
 </style>
---
+++ src/components/Workout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="workout-wrapper">
+    <WorkoutHeader/>
+
+    <div class="info-callout-wrapper">
+      <Callout
+        v-for="callout in calloutData"
+        :key="callout.title"
+        :title="callout.title"
+        :listItems="callout.listItems"
+      />
+    </div>
+
+    <ExerciseTable
+      :tableTitlesList="workoutTableData.titlesList"
+      :exerciseList="workoutTableData.exercises"
+      :weightColumnColespan=workoutTableData.weightColumnColespan
+    />
+  </div>
+</template>
+
+<script>
+import WorkoutHeader from './WorkoutHeader.vue'
+import Callout from './Callout.vue'
+import ExerciseTable from './ExerciseTable.vue'
+
+export default {
+  name: 'Workout',
+  components: {
+    WorkoutHeader,
+    Callout,
+    ExerciseTable
+  },
+  props: {
+    calloutData: Object,
+    workoutTableData: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .workout-wrapper {
+    margin: 1rem 0 2.5rem;
+  }
+</style>
